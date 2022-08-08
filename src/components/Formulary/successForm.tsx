e475828675982937d48a9e3d56a3da869ee66226
nclsgg <nicolasguadagno@gmail.com>
2022-08-08 14:47:39 -0300

    Setting up Project

---
+++ src/components/Formulary/successForm.tsx
@@ -0,0 +1,10 @@
+function SuccessForm() {
+    return (
+        <div className="success-form">
+        <h1>Thank You!</h1>
+        <p>
+            We've added your card details
+        </p>
+        </div>
+    );
+}
